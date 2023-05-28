d01cb1a6505925a96db20ac916671d1a0c9f3962
BandsThoBaby <32070633+BandsThoBaby@users.noreply.github.com>
2023-05-28 17:42:07 +0100

    Add files via upload
    Python Lists are Ordered, Indexed, Changeable and allow duplicates

---
+++ 28may23_lists.py
@@ -0,0 +1,31 @@
+
+"""
+Lists allow us to store multiple values in a  single variable. 
+
+Lists are: Ordered | Changeable | Allow duplicate values 
+"""
+
+a_new_list = ["papaya", "mango", "pineapple"]
+print(a_new_list)
+
+# List items are indexed. (Starting at [0] of course!!!!)
+
+# If you add new items to a list, they will be placed at the end of the list. 
+
+# Because a list is changebale we can: change, add and remove values after a list has been created. 
+
+# Since lists are index they allow items to have the same values. 
+
+# We can make list items from any data type: Int, boolean, float... whatever you want...
+
+string_list = ["Bangkok", "Buriram", "Phuket"]
+int_list = [4, 6, 9, 2]
+bool_list = [True, False, False]
+
+# A list can also be created using the list() constructor:
+
+july_plans = list(("Tate_Modern", "Magic_Mushrooms", 5000, True
+
+))
+
+print(july_plans)
